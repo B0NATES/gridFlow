b4f72605b2c8eac5d8dc23764eeb70ca1ed7038d
Bonates <natanbonatescursos@hotmail.com>
2025-11-05 01:58:47 -0300

    correção na chamada da api

--- front/api/apiService.js
+++ front/api/apiService.js
@@ -1,6 +1,6 @@
 // front/api/apiService.js
 
-const API_BASE_URL = "https://gridflow-api.onrender.com";
+const API_BASE_URL = "https://gridflow-api.onrender.com/api";
 
 // 🔹 Login do usuário
 export async function login(email, senha) {
