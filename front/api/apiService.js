9a9781e13df8b30005e6054968bf1942c3c0f3c6
Bonates <natanbonatescursos@hotmail.com>
2025-11-05 02:02:15 -0300

    teste

--- front/api/apiService.js
+++ front/api/apiService.js
@@ -1,6 +1,6 @@
 // front/api/apiService.js
 
-const API_BASE_URL = "https://gridflow-api.onrender.com/api";
+const API_BASE_URL = "https://gridflow-api.onrender.com";
 
 // 🔹 Login do usuário
 export async function login(email, senha) {
