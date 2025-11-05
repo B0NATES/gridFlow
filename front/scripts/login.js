c82ed0146259d314639cf5c14d320e5bdb42e764
Bonates <natanbonatescursos@hotmail.com>
2025-11-05 01:46:25 -0300

    correção na rota de login

--- front/scripts/login.js
+++ front/scripts/login.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:4000/api";
+const API_BASE_URL = "https://gridflow-api.onrender.com";
 
 document.getElementById("login-form").addEventListener("submit", async (e) => {
   e.preventDefault();
